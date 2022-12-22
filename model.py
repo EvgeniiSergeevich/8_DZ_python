46660dbfdbd87444191cd06073eb5ae5d2650ef6
Evgeniy <zheinirk38@gmail.com>
2022-12-22 19:19:56 +0800

    Добавил файлы, добавил методы в model

---
+++ model.py
@@ -0,0 +1,13 @@
+def add_people(file, data):
+    
+    return 0
+
+def remove_people(file, data):
+    return 0
+
+def find_people(file, data):
+    return 0
+
+def edit_people(file, data, new_data):
+    return 0
+
